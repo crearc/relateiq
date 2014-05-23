b8d20a61ff348d650337680f4f1b5f25bc2e9d05
Tristan Wietsma <tristan.wietsma@gmail.com>
2014-05-22 23:56:23 -0500

    added models

---
+++ relateiq/models.py
@@ -0,0 +1,21 @@
+class Organization:
+    pass
+
+class Contacts:
+    pass
+
+class Accounts:
+    pass
+
+class Lists:
+    pass
+
+class ListItem:
+    pass
+
+class Events:
+    pass
+
+class User:
+    pass
+
