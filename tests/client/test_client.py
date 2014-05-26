0cdbba28bb2c147449704632ba4d97d0ae2020e5
Tristan Wietsma <tristan.wietsma@gmail.com>
2014-05-25 21:23:07 -0500

    added test skeleton for client

---
+++ tests/client/test_client.py
@@ -0,0 +1,67 @@
+class TestClient(object):
+
+    def setup(self):
+        pass
+
+    def test_init(self):
+        pass
+
+    def test_request(self):
+        pass
+        
+    def test_organizations(self):
+        pass
+
+    def test_contacts(self):
+        pass
+
+    def test_create_contact(self):
+        pass
+
+    def test_get_contact(self):
+        pass
+
+    def test_update_contact(self):
+        pass
+
+    def test_accounts(self):
+        pass
+
+    def test_create_account(self):
+        pass
+
+    def test_get_account(self):
+        pass
+
+    def test_update_account(self):
+        pass
+
+    def test_lists(self):
+        pass
+
+    def test_get_list(self):
+        pass
+
+    def test_list_items(self):
+        pass
+
+    def test_create_list_item(self):
+        pass
+
+    def test_get_list_item(self):
+        pass
+
+    def test_update_list_item(self):
+        pass
+
+    def test_delete_list_item(self):
+        pass
+
+    def test_events(self):
+        pass
+
+    def test_create_event(self):
+        pass
+
+    def test_users(self):
+        pass
